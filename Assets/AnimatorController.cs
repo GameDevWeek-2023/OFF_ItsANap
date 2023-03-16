066a4d025293eb26c7828c823441cbcab23a477e
s4safeld <sascha.feld@gmx.net>
2023-03-16 16:48:36 +0100

    playerAnimation

--- Assets/AnimatorController.cs
+++ Assets/AnimatorController.cs
@@ -6,13 +6,11 @@ using UnityEngine;
 
 public class AnimatorController : MonoBehaviour
 {
-    private Player player;
+    public Player player;
     public Animator animator;
 
     private void Start()
     {
-        player = GetComponent<Player>();
-
         player.jump += JumpAnim;
     }
 
@@ -21,20 +19,20 @@ public class AnimatorController : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) {
             Debug.Log("a");
            animator.SetBool("walking", true);
-           transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+           transform.localScale = new Vector3(-0.25f, transform.localScale.y, transform.localScale.z);
         }
         if (Input.GetKey(KeyCode.D)) {
             Debug.Log("d");
             animator.SetBool("walking", true);
-            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(0.25f, transform.localScale.y, transform.localScale.z);
         }
 
 
-        if(GetComponent<Rigidbody2D>().velocity.y < 0.01f && player.onGround())
+        if(player.gameObject.GetComponent<Rigidbody2D>().velocity.y < 0.01f && player.onGround())
             animator.SetBool("jumping", false);
         
         
-        if(GetComponent<Rigidbody2D>().velocity.x < 0.01f)
+        if(player.gameObject.GetComponent<Rigidbody2D>().velocity.x < 0.01f)
             animator.SetBool("walking", false);
     }
 
