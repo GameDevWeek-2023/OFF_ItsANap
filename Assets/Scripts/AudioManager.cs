c3092215662dd28d9aebc6a23e25db7c52de3844
Daniel <daniel.soelch@web.de>
2023-03-17 18:31:36 +0100

    added sound files

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer GameAudioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetEffectSoundVolume(float volume) {
+        GameAudioMixer.SetFloat("EffectsVolume", volume);
+    }
+
+
+    // 2DO: Methode zur Anpassung der Musiklautstärke implementieren
+    public void SetMusicVolume(float volume) {
+        GameAudioMixer.SetFloat("MusicVolume", volume);
+    }
+
+}
