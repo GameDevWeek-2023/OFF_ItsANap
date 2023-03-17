c9de2a2a1e5e5677b4a680d15b9411c39969e154
s4safeld <sascha.feld@gmx.net>
2023-03-17 13:21:51 +0100

    Fixed Player Death and added SandwichmakerCode

--- Assets/Scripts/ScriptsSascha/SignFallOver.cs
+++ Assets/Scripts/ScriptsSascha/SignFallOver.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SignFallOver : MonoBehaviour
 {
     private bool fallOver;
-    public GameObject bla;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -21,6 +21,10 @@ public class SignFallOver : MonoBehaviour
         if (transform.localEulerAngles.x < 80 && fallOver)
         {
             transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime*(transform.localEulerAngles.x+1)/10);
+        }
+        else
+        {
+
         }
     }
 }
